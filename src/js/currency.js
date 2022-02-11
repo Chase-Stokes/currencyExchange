b123f1ad0402ed736ce428f258fb54fd94342634
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-11 09:12:26 -0800

    install npm and make an api call

---
+++ src/js/currency.js
@@ -0,0 +1,13 @@
+export default class exchangeAPI {
+  static async getCurrencyAndRate() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/GBP/AMOUNT`);
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json;
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
