17c63d833d925ca636002cc303ea446d497d586c
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-11 10:59:19 -0800

    update the js to store the api response, and html form values

--- src/index.js
+++ src/index.js
@@ -3,3 +3,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeAPI from './js/currency.js';
+
+function getAndDisplay(response, amount, target) {
+
+}
+
+async function makeApiCall(amount, target) {
+  const response = await ExchangeAPI.getCurrencyAndRate();
+  getAndDisplay(response, amount, target);
+}
+
+$('document').ready(function(){
+  
+  $('#formOne').submit(function(event){
+    event.preventDefault();
+    let amount = $('#amount').val();
+    let target = $('target').val();
+    makeApiCall(amount, target);
+
+  })
+})
