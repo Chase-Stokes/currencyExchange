dbf77a7933a71265dff18d8e698b4b79f76cf529
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-11 14:08:23 -0800

    add functionality for the user to choose their initial currencyto convert from

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeAPI from './js/currency.js';
 
-function getAndDisplay(response, amount, target) {
+function getAndDisplay(response, amount, target, baseCode) {
   if (response.result) {
-    $('.output').text(`$${amount} USD, would be ${Math.round(response.conversion_result)} in ${target}. With a conversion rate of ${response.conversion_rate}.`);
+    $('.output').text(`$${amount} ${baseCode}, would be ${response.conversion_result} in ${target}. With a conversion rate of ${response.conversion_rate}.`);
   } else {
     $('.errors').text(`There was an error: ${response}`);
   }
 }
 
-async function makeApiCall(amount, target) {
-  const response = await ExchangeAPI.getCurrencyAndRate(amount, target);
-  getAndDisplay(response, amount, target);
+async function makeApiCall(amount, baseCode, target) {
+  const response = await ExchangeAPI.getCurrencyAndRate(amount, baseCode, target);
+  getAndDisplay(response, amount, target, baseCode);
 }
 
 $('document').ready(function(){
@@ -22,9 +22,10 @@ $('document').ready(function(){
   $('#formOne').submit(function(event){
     event.preventDefault();
     let amount = $('#amount').val();
+    let baseCode = $('#baseCode').val();
     let target = $('#target').val();
-    console.log(amount, target)
-    makeApiCall(amount, target);
+    makeApiCall(amount, baseCode, target);
+    $('.box1').show();
 
   })
 })
--- src/js/currency.js
+++ src/js/currency.js
@@ -1,7 +1,7 @@
 export default class ExchangeAPI {
-  static async getCurrencyAndRate(amount, target) {
+  static async getCurrencyAndRate(amount, target, baseCode) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${target}/${amount}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCode}/${target}/${amount}`);
       if(!response.ok) {
         throw Error(response.statusText);
       }
